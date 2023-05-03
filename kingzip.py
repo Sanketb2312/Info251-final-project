b697653ef8779d4c41fc5b9e9d030cee22bc34b6
Dherik <jendeva@hotmail.com>
2023-05-02 18:42:39 -0700

    Made graph of house price increases

--- kingzip.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("Zip_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv")
-king_county_data = data[data["CountyName"] == "King county"]
-print(king_county_data)
